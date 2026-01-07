f4723855702f9b63b005cfeb98e4588134051de0
Daniel <dan@gobloom.io>
2026-01-07 13:27:31 -0700

    Adds to pages router too

--- pages/_app.js
+++ pages/_app.js
@@ -34,6 +34,13 @@ function MyApp({ Component, pageProps }) {
           </Script>
         </>
       )}
+      <Script
+        id="crisp-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `window.$crisp=[];window.CRISP_WEBSITE_ID="7af76858-a346-4774-91d7-fd99602a4d47";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();`,
+        }}
+      />
     </>
   );
 }
